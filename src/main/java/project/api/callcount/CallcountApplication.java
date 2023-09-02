8f37341213b120b3c33fa3398bb16f4a5762ed4e
devjun10 <devjun1023@gmail.com>
2023-09-03 01:20:43 +0900

    chore: project build
    - 프로젝트 빌드
     *Caffeine 의존성 추가

---
+++ src/main/java/project/api/callcount/CallcountApplication.java
@@ -0,0 +1,13 @@
+package project.api.callcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallcountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallcountApplication.class, args);
+    }
+
+}
