bc70e7391f5c2604543eb66184ffdae386d8ec98
devjun10 <devjun1023@gmail.com>
2023-09-03 01:51:44 +0900

    feature: cache-evict api development

--- src/main/java/project/api/callcount/core/web/presentation/ExternalApiCallController.java
+++ src/main/java/project/api/callcount/core/web/presentation/ExternalApiCallController.java
@@ -1,5 +1,8 @@
 package project.api.callcount.core.web.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import project.api.callcount.core.web.outter.ExternalPort;
 @Controller
 public class ExternalApiCallController {
 
+    private static final Logger log = LoggerFactory.getLogger(ExternalApiCallController.class);
     private final ExternalPort externalURL;
 
     public ExternalApiCallController(ExternalPort externalURL) {
@@ -17,12 +21,21 @@ public class ExternalApiCallController {
     }
 
     @GetMapping("/external")
-    @Cacheable(cacheNames = "apiCache")
+    @Cacheable(cacheNames = "apiCache", key = "#root.method.name")
     public ResponseEntity<String> callExternalAPI() {
+        log.info("Call external api.");
         String htmlWebPage = externalURL.callExternal();
         return ResponseEntity.ok()
             .contentType(MediaType.TEXT_HTML)
             .body(htmlWebPage);
+    }
+
+    @GetMapping("/evict-cache")
+    @CacheEvict(value = "apiCache", key = "'callExternalAPI'")
+    public ResponseEntity<Void> evictCache() {
+        log.info("Evict apiCache.");
+        return ResponseEntity.ok()
+            .build();
     }
 }
 
